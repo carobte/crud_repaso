bad51b5cd8b334949b0d1c287f29415240451ccd
carobte <caro.bustamante.escobar@gmail.com>
2024-05-31 13:38:43 -0500

    feat: register & login

---
+++ src/js/login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form-login")
+const password = document.querySelector("#password")
+const email = document.querySelector("#email")
+const API = "http://localhost:3000/users"
+
+async function validateEmail(email) {
+    const response = await fetch(`${API}?email=${email}`)
+    const data = await response.json()
+    if (data.length > 0) {
+        return data[0]
+    } else {
+        return false
+    }
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+    const user = await validateEmail(email)
+    if (!user){
+        alert("User not registred")
+    } else {
+        if (user.password === password.value) {
+            localStorage.setItem("user", JSON.stringify(user))
+            alert("Logged in successfully")
+        }
+    }
+})
--- src/js/register.js
+++ src/js/register.js
@@ -1,19 +1,62 @@
-const name = document.querySelector("#name")
+const userName = document.querySelector("#name")
 const lastName = document.querySelector("#last-name")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 const confirmPassword = document.querySelector("#confirm-password")
 const registerForm = document.querySelector("#register-form")
+const API = "http://localhost:3000/users"
+
+// Validar contraseña
 
 function validatePassword(password, confirmPassword) {
     if (password.value === confirmPassword.value) {
         return true
     } else {
+        alert("Invalid password")
         return false
     }
 }
 
+// Validar si el usuario existe 
 
-registerForm.addEventListener("submit", (event)=>{
+async function validateUser(email) {
+    const response = await fetch(`${API}?email=${email.value}`)
+    const data = await response.json()
 
+    if (data.length === 0) {
+        return true
+    } else {
+        alert("User already exists")
+        return false
+    }
+}
+
+// Crear usuario
+
+async function createUser(name, lastName, email, password) {
+    const newUser = {
+        name: name.value,
+        lastName: lastName.value,
+        email: email.value,
+        password:  password.value
+    }
+
+    await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+}    
+
+
+registerForm.addEventListener("submit", async (event) => {
+    event.preventDefault()
+    const samePassword = validatePassword(password, confirmPassword)
+    const userExists = await validateUser(email)
+    if (samePassword && userExists) {
+        await createUser(userName, lastName, email, password)
+        alert("User created successfully")
+    }
 })
